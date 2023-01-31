9563e48b017c724bafe0ea5ee3c2bd6945b37cef
Gulzat <gulzat.aldashova.ch@gmail.com>
2023-01-31 18:49:18 +0600

    Added some new class

--- src/main/java/practice/PracticeDemo.java
+++ src/main/java/practice/PracticeDemo.java
@@ -3,5 +3,6 @@ package practice;
 public class PracticeDemo {
     public static void main(String[] args) {
         System.out.println("Thank you for inviting");
+        System.out.println("New class");
     }
 }
