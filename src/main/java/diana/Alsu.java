5b142afceef2c84e2e1d156bf7508be6ac3fa97a
Diana <abdyrahunovadiana@gmail.com>
2023-02-10 18:14:02 +0300

    Added new class

---
+++ src/main/java/diana/Alsu.java
@@ -0,0 +1,10 @@
+package diana;
+
+public class Alsu {
+    public static void main(String[] args) {
+        System.out.println("Heloo");
+        System.out.println("Meoooow");
+        System.out.println("My name");
+
+    }
+}
--- src/main/java/diana/MyFirstGit.java
+++ src/main/java/diana/MyFirstGit.java
@@ -6,5 +6,6 @@ public class MyFirstGit {
         System.out.println("I am Diana");
         System.out.println("Hello world");
         System.out.println("MY dog");
+        System.out.println("hi");
     }
 }
