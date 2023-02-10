6f15271f3ddaa028b62135c2dd0e20ef31031d7d
Diana <abdyrahunovadiana@gmail.com>
2023-02-10 17:43:13 +0300

    Some change

--- src/main/java/diana/MyFirstGit.java
+++ src/main/java/diana/MyFirstGit.java
@@ -4,5 +4,6 @@ public class MyFirstGit {
     public static void main(String[] args) {
         System.out.println("I am a tester");
         System.out.println("I am Diana");
+        System.out.println("Hello world");
     }
 }
